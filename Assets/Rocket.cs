141fb9fd58643d7880e71816df2344f7dda0b6ca
lovedakoko <dkoko002@gmail.com>
2020-09-02 15:11:58 -0400

    First attempt at making rocket sounds happen by working with audio source and documentation

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,31 +5,63 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+
+    //Declare your member variables (make sure that is the right name)
+    Rigidbody rigidBody;
+    AudioSource audioSource;
+    bool m_Play; //Play the music
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //New piece of code: allows you to extract component of the game object to which your script is attached.
+        //In this case, you are getting the component (get component) of your object related to it's rigid body (<Rigidbody>)
+        //characteristics. From there you can manipulate the physics of that rigid body
+        //update: you have also extracted the AudioSource component
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+
+        //Ensure the toggle is set to true for the music to play at start-up
+        m_Play = true;
     }
 
     // Update is called once per frame
     void Update()
     {
         ProcessInput();
+        RocketNoise();
+    }
+
+    private void RocketNoise()
+    {
+        if (m_Play == true)//Check to see if you just set toggle to positive
+        {
+            audioSource.Play();//Play the audio you attach to the AudioSource component
+        }
+        if (m_Play == false)//Check if you just set the toggle to false
+        {
+            audioSource.Stop();//Stop the audio
+        }
     }
 
     private void ProcessInput()
     {
         if (Input.GetKey(KeyCode.Space)) // can thrust while rotating
         {
-            print("Thrusting");
+            rigidBody.AddRelativeForce(Vector3.up);
+            m_Play = true;
         }
         if (Input.GetKey(KeyCode.A))
         {
-            print("A pressed means rotate left");
+            transform.Rotate(Vector3.forward);
         }
         else if(Input.GetKey(KeyCode.D))
         {
-            print("D pressed means rotate right");
+            transform.Rotate(Vector3.back);
+        }
+        else
+        {
+            m_Play = false;
         }
     }
 }

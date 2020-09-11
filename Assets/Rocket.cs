ec1a5f3baf5170cdfd63932cc1e872e06159deeb
lovedakoko <dkoko002@gmail.com>
2020-09-11 15:49:49 -0400

    So much... added custom audio, particle effects, level loading,a lot... still having trouble with audio clipping sounds though

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -1,8 +1,13 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.SceneManagement; //gives access to unity scene managment code
+//deals with namespaces, which requires understanding classes --> future sections!!
 
+
+//TODO: fix lighting bug
 public class Rocket : MonoBehaviour
 {
 
@@ -12,6 +17,21 @@ public class Rocket : MonoBehaviour
     //bool m_Play; //Play the music
     [SerializeField] float rcsThrust = 120f;
     [SerializeField] float thrustThisFrame = 750f;
+    
+    //Audioclips here
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip booster;
+    [SerializeField] AudioClip death;
+    [SerializeField] AudioClip victory;
+
+    //Particle Effects here
+    [SerializeField] ParticleSystem mainEngineParticles;
+    [SerializeField] ParticleSystem deathParticles;
+    [SerializeField] ParticleSystem victoryParticles;
+
+    enum State {Alive, Dead, Transcending};
+    State state = State.Alive;
+
 
     // Start is called before the first frame update
     void Start()
@@ -27,26 +47,27 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Thrust();
-        Rotate();
+        // somwhere, stop sound on death
+        if (state == State.Alive)
+        {
+            RespondtoThrustInput();
+            RespondtoRotateInput();
+        }
     }
 
     void OnCollisionEnter(Collision collision)
     {
+        if (state != State.Alive) { return; } //means stop execution of this function at this point
         switch (collision.gameObject.tag)
         {
             case "Friendly":
                 //do nothing
-                print("OK"); //todo remove
                 break;
-            case "Fuel":
-                print("Fuel");
-                break;
-            case "Damage":
-                print("Damage");
+            case "Finish":
+                StartVictorySequence();
                 break;
             default:
-                print("dead");
+                StartDeathSequence();
                 //kill player
                 break;
         }
@@ -59,29 +80,68 @@ public class Rocket : MonoBehaviour
         //    audioSource.Play();
         //}
     }
-    private void RocketNoiseOn()
+
+    private void StartDeathSequence()
+    {
+        state = State.Dead;
+        audioSource.Stop();
+        mainEngineParticles.Stop();
+        audioSource.PlayOneShot(death);
+        deathParticles.Play();
+        Invoke("LoadFirstLevel", 1f);
+    }
+
+    private void StartVictorySequence()
+    {
+        state = State.Transcending;
+        audioSource.Stop();
+        mainEngineParticles.Stop();
+        audioSource.PlayOneShot(victory);
+        victoryParticles.Play();
+        Invoke("LoadNextScene", 5f); //parametrize time
+    }
+
+    private void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    private void LoadNextScene()
+    {
+        SceneManager.LoadScene(1); //todo allow for more than 2 levels
+    }
+
+    private void RocketEffectsOn()
     {
         if (!audioSource.isPlaying)//So it doesn't layer
         {
-            audioSource.Play();//Play the audio you attach to the AudioSource component
+            audioSource.PlayOneShot(mainEngine);//Play the audio you attach to the AudioSource component
+            audioSource.PlayOneShot(booster);
+            mainEngineParticles.Play();
         }
     }
 
-    private void Thrust()
-    {
-        float mainThrust = thrustThisFrame * Time.deltaTime;
+    private void RespondtoThrustInput()
+    { 
         if (Input.GetKey(KeyCode.Space)) // can thrust while rotating
         {
-            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
-            RocketNoiseOn();
+            ApplyThrust();
         }
         else
         {
             audioSource.Stop();//Stop the audio
+            mainEngineParticles.Stop();
         }
     }
 
-    private void Rotate()
+    private void ApplyThrust()
+    {
+        float mainThrust = thrustThisFrame * Time.deltaTime;
+        rigidBody.AddRelativeForce(Vector3.up * mainThrust);
+        RocketEffectsOn();
+    }
+
+    private void RespondtoRotateInput()
     {
         rigidBody.freezeRotation = true; //take manual control of rotation
         float rotationThisFrame = rcsThrust * Time.deltaTime;
